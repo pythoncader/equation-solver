3dd8d7a88c2d8590fdd1697d9a09e148f4dd98cc
pythoncader <caderigby@gmail.com>
2022-01-21 10:57:49 -0700

    start on object oriented programming

---
+++ object-oriented/equation-class.py
@@ -0,0 +1,12 @@
+class equation:
+    def __init__(self, equation): #initiate the object with a string equation
+        self.equation
+
+    def split():
+        #split into terms
+
+    def orderofops():
+        #determine how to solve the equation in correct order
+    
+    def solve():
+        #perform inverse operations to get x value
